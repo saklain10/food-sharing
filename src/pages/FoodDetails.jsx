8b46810f52c0fd806d84db57fe7a57c96bc04550
Saklain Mostak <saklainmostak926@gmail.com>
2025-08-10 14:35:06 +0600

    final commit

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -232,7 +232,7 @@ const FoodDetails = () => {
                 </motion.button>
                 <h3 className="text-2xl font-extrabold mb-4 text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-500">Confirm Your Request</h3>
 
-                {/* Grid for form fields .*/}
+                {/* Grid for form fields ..*/}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-3 gap-x-4 mb-4">
                   <div>
                     <label className="block text-gray-700 text-sm font-semibold mb-1">Food Name</label>
